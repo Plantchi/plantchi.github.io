091bde11295cc7fffd4ba49c254ea7b78bbea669
kelsey-debug <kelsey36@pdx.edu>
2021-02-18 19:07:52 -0800

    changed from websockets to socketio

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 /*local port is set to :   http://localhost:3000
  */
 const express = require('express')
-const {Server} = require('ws');
+const socketIO = require('socket.io')
+//const {Server} = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+//create http server
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
 //web socket server takes an http server as an arguement to listen to events on client side (reqs)
-const wss = new Server ({ server }) 
+//const wss = new Server ({ server }) 
 
-wss.on('connection',(ws) => {
-    console.log('Client Connected');
-    ws.on('close', () => console.log('Client has disconnected'))
-})
+//socket IO takes http server as arg to listen to socketIO requests
+const io = socketIO(server);
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
