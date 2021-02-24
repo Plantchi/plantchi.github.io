6f602578798d6b10901924c9efb54acc4135799d
kelsey-debug <kelsey36@pdx.edu>
2021-02-24 13:52:13 -0800

    procfile, css working, cleaned up server code, renamed files

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-/*local port is set to :   http://localhost:3000
+/*local port is set to :   http://localhost:3000 
  */
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 3000;
-const INDEX = 'index.html';
-const HOWTO = 'howto.html';
+ //module requests/consts
+var express = require('express');
+var socket = require('socket.io'); 
+const HOWTO = 'public/howto.html';
 
-//app.use(express.static(__dirname + '/public/'));
+
+//set up app 
+var app = express();
+var server = app.listen(3000, function(){
+  console.log('Listening on 3000');
+});
+
+//static files for retrieval
+app.use(express.static('public'));
+
+//pass the socket a server. "i want socketio to work on this server"
+var io = socket(server);
 
 //cloudinary
 
@@ -22,19 +31,6 @@ cloudinary.config({
 // cloudinary.url("tree.png"), {width: 100, height: 150, crop: "fill"}
 //end of cloudinary
 
-/*create http server
-//create http server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-*/
-
-//socket IO takes http server as arg to listen to socketIO requests
-//(attach socket.io to our http server)
-//const io = socketIO(server);
-app.get('/', (req,res) =>{
-  res.sendFile(INDEX, { root: __dirname });
-})
 
 app.get('/howto', (req,res) =>{
   res.sendFile(HOWTO, { root: __dirname });
@@ -44,7 +40,8 @@ app.get('/tree', (req,res) =>{
   res.sendFile(HOWTO, { root: __dirname }); //make a page for the tree interaction aka general care page with tree photo 
 })
 
-io.on('connection', (socket) => {
+
+io.on('connection', (socket) => { //"socket" refers to socket made for this instance -aka each client has own socket
   console.log('Client connected');
  
   socket.on('createRoom', room => {
@@ -61,8 +58,3 @@ io.on('connection', (socket) => {
 //test function
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-
-
-http.listen(3000, ()=>{
-  console.log(`Listening on 3000`);
-});
